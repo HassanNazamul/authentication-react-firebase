322f708114e6b3d21a0f077b3c102b7d9d56e387
Mohammed <hassan.nazamul05@gmail.com>
2024-07-04 22:25:05 -0400

    router corrected

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/authentication-react-firebase">
       <Routes>
         <Route path="/" element={<Login />} ></Route>
         <Route path="/signup" element={<Signup />}></Route>
