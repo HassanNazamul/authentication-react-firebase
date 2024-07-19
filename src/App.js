1146a1a6aacf6b5f4024391616a64a3819cbc687
Mohammed <hassan.nazamul05@gmail.com>
2024-07-18 21:29:07 -0400

    database correction

--- src/App.js
+++ src/App.js
@@ -10,11 +10,17 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
+    // BrowserRouter is the router component for web applications
+    // basename sets the base URL for all locations
     <BrowserRouter basename="/authentication-react-firebase">
       <Routes>
+        {/* Route for the login page */}
         <Route path="/" element={<Login />} ></Route>
+
+        {/* Route for the signup page */}
         <Route path="/signup" element={<Signup />}></Route>
 
+        {/* Protected route for the profile page */}
         <Route path="/profile" element={
           <ProtectedRoute>
             <Profile />
@@ -22,12 +28,15 @@ function App() {
         }
         ></Route>
 
+        {/* Catch-all route for undefined paths, displaying the Error component */}
         <Route path="*" element={<Error />} />
       </Routes>
+
+      {/* ToastContainer to show toast notifications */}
       <ToastContainer
-        transition={Slide}
-        position="top-right"
-        autoClose={5000}
+        transition={Slide}          
+        position="top-right"        
+        autoClose={5000}            
       />
     </BrowserRouter>
   );
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Ensure toastify CSS is imported
 
 const Error = () => {
-    const navi = useNavigate();
+    // useNavigate hook to navigate
+    const navigate = useNavigate();
 
+    // useEffect hook to perform side effects
     useEffect(() => {
+        // Display an error toast notification with a custom message and autoClose duration
         toast.error("Invalid Entry", { autoClose: 1000 });
-        navi("/");
-    }, []);
+        // Navigate to the home page ("/") after showing the toast
+        navigate("/");
+    }, [navigate]); // Add navigate as a dependency
 
-
-
-    return (
-
-        <></>
-    );
+    // Return an empty fragment as the component doesn't need to render any UI
+    return <></>;
 }
 
-export default Error
+export default Error;
